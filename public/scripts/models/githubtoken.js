77c42913aec2f67b38382d6ccae158d3f7c4a708
Michael Padget <mpadget@comcast.net>
2017-03-29 17:28:45 -0700

    Added git token, ajax call, and api objects

---
+++ public/scripts/models/githubtoken.js
@@ -0,0 +1,3 @@
+'use strict';
+
+const githubToken = 'b57809b1fcfe0f038efcc1e2923072698876620d';
--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -7,8 +7,25 @@
 
   repos.requestRepos = function(callback) {
     // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    $.ajax({
+      url: 'https://api.github.com/users/mmpadget/repos',
+      method: 'GET',
+      headers: {
+        Authorization: `token ${githubToken}`
+      }
+    })
+    .then(
+      data => data.forEach(repo => $('#repo-template').append(repo.name)),
+      data => data.forEach(repo => $('#repo-template').append(repo.html_url)),
+      data => data.forEach(repo => $('#repo-template').append(repo.description)),
+      data => data.forEach(repo => $('#repo-template').append(repo.language)),
+      data => data.forEach(repo => $('#repo-template').append(repo.created_at)),
+      data => data.forEach(repo => $('#repo-template').append(repo.updated_at)),
+      data => data.forEach(repo => $('#repo-template').append(repo.watchers_count))
 
-  };
+      err => console.log(err)
+    )
+};
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
