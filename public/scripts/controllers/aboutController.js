b0891a5b7810c0ee23310d0a5de838e593304870
Chris Closser <chrisclosser1@gmail.com>
2017-03-30 11:55:11 -0700

    working

--- public/scripts/controllers/aboutController.js
+++ public/scripts/controllers/aboutController.js
@@ -8,7 +8,8 @@
 
     // TODO: Call a function to load all the data.
     // Pass a view function as a callback, so the view will render after the data is loaded.
-    repos.requestRepos();
+    repos.requestRepos(repoView.index);
+
   };
 
   module.aboutController = aboutController;
